fc11aefdb32baf44ef29a9830c85b145d82fbb08
Данила Тяпкин <dtapkin6@gmail.com>
2023-10-21 12:31:24 +0300

    Добавьте файлы проекта.

---
+++ DownloadPicture/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        HttpClient client = new();
+
+        Console.WriteLine("Введите ссылку для скачивания картинки: ");
+        string nameWebsite = Console.ReadLine();
+
+        HttpResponseMessage response = await client.GetAsync(nameWebsite);
+        byte[] data = await response.Content.ReadAsByteArrayAsync();
+
+        await File.WriteAllBytesAsync("C:\\Users\\DELL\\Downloads\\image.jpg", data);
+
+       Process.Start(new ProcessStartInfo { FileName = "C:\\Users\\DELL\\Downloads\\image.jpg", UseShellExecute = true });
+    }
+}
